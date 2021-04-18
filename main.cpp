110b16911f6b4565c44dfa8be7ad770f21026871
musasimsekdotdll <musa.simsek@boun.edu.tr>
2021-04-19 00:58:39 +0300

    hata duzeltildi

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 using namespace std;
 
 
-stack<int> operands;
 stack<char> ops;
 
 int preced(char c) {
@@ -37,8 +36,6 @@ bool isOperator(string token) {
 		case'-':
 		case'*':
 		case'/':
-		case'(':
-		case')':
 			return true;
 			break;
 		default: 
@@ -74,76 +71,42 @@ int result(int a, int b, char c) {
 
 void compute(vector<string>& tokens, string& postfix) {
 
-	int count = 0;
+	char op;
 	for(int i=0; i<tokens.size(); i++) {
-		cout << i << endl;
 		if(isOperand(tokens[i])) {
-			cout << i << " in isOperand" << endl;
-			operands.push(stoi(tokens[i]));
-			cout << i << " in isOperand2" << endl;
+			postfix += tokens[i];
 		} else if(tokens[i] == "(") {
 			ops.push('(');
 		} else if(tokens[i] == ")") {
 
 			while(!ops.empty() && ops.top() != '(') {
-				int v1, v2;
-				char op;
 
-				v1 = operands.top();
-				operands.pop();
-				v2 = operands.top();
-				operands.pop();
 				op = ops.top();
 				ops.pop();
 
-				if(count == 0) {
-					postfix += to_string(v2);
-				}
-
-				postfix += to_string(v1);
 				postfix += " ";
-				// cout << postfix << endl;
 				postfix[postfix.length()-1] = op;
 
-				int res = result(v2, v1, op);
-				cout << res << endl;
-				operands.push(res);
-				count++;
 			}
 			
 			if(!ops.empty()) {
 				ops.pop();
 			}
 		} else if(isOperator(tokens[i])) {
-			// cout << i << endl;
+
 			if(ops.empty() || ops.top() == '(') {
 				ops.push(tokens[i][0]);
 			} else {
 
 				while(!ops.empty() && ops.top() != '(' && preced(tokens[i][0]) <= 
 															preced(ops.top())) {
-					int v1, v2;
-					char op;
 
-					v1 = operands.top();
-					operands.pop();
-					v2 = operands.top();
-					operands.pop();
 					op = ops.top();
 					ops.pop();
 
-					if(count == 0) {
-						postfix += to_string(v2);
-					}
-
-					postfix += to_string(v1);
 					postfix += " ";
 					postfix[postfix.length()-1] = op;
 
-					int res = result(v2, v1, op);
-					operands.push(res);
-					count++;
-					// cout << res << endl;
 				}
 
 				ops.push(tokens[i][0]);
@@ -152,36 +115,15 @@ void compute(vector<string>& tokens, string& postfix) {
 
 	}
 
-	cout << "last step" << endl;
 
 	while(!ops.empty()) {
-		cout << "opssize : " << ops.size() << endl;
-		int v1, v2;
-		char op;
 
-		cout << "opersize:" << operands.size() << endl;
-		v1 = operands.top();
-		operands.pop();
-		cout << v1 << endl;
-		v2 = operands.top();
-		operands.pop();
-		cout << v2 << endl;
 		op = ops.top();
 		ops.pop();
-		cout << "op:" << op << endl;
 
-		if(count == 0) {
-			postfix += to_string(v2);
-		}
-
-		postfix += to_string(v1);
 		postfix += " ";
 		postfix[postfix.length()-1] = op;
 
-		int res = result(v2, v1, op);
-		// cout << res << endl;
-		operands.push(res);
-		count++;
 	}
 }
 
@@ -192,13 +134,9 @@ int main(int argc, char const *argv[])
 {
 
 	string post = "";
-	// post += 5;
-	// // post += 3;
-	// // post += " ";
-	// // post[post.length()-1] = '*';
-	vector<string> line = { "12", "+", "3", "*", "5"};
+
+	vector<string> line = { "(", "12", "-", "10", ")", "*", "(", "1", "-" , "3", ")"};
 	compute(line, post);
-	cout << operands.top() << endl;
 	cout << post << endl;
 	return 0;
 }
